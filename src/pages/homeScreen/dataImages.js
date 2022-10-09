5b13f004feaf053653f196b24cf55233c0f427b4
Januacele <januacelevieira@hotmail.com>
2022-10-09 14:14:12 -0300

    feat: insert data images list for home screen

---
+++ src/pages/homeScreen/dataImages.js
@@ -0,0 +1,30 @@
+export const dataImagesHome = [
+    {
+      img: 'https://aguasbelas.pe.gov.br/wp-content/uploads/2022/04/eu-amo-aguas-belas-1200x720.jpg',
+      title: 'Asfalto Rua Nova ',
+    },
+    {
+      img: 'https://i.pinimg.com/474x/4f/11/50/4f1150328bd14a2b9c26e28aa066086a.jpg',
+      title: 'Pracinha Rua Alfredo Duarte',
+    },
+    {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWxVUC8MgoSqd2q_iIwtcgCLgGd3ypWnyp_A&usqp=CAU',
+      title: 'Praça Central',
+    },
+    {
+      img: 'https://aguasbelas.pe.gov.br/wp-content/uploads/2021/03/Prefeitura-de-Aguas-Belas-1200x694.jpg',
+      title: 'Prefeitura',
+    },
+    {
+      img: 'https://static.consolidesuamarca.com.br/data/images/aguas-belas-pe.jpg?18092019',
+      title: 'Cruzeiro',
+    },
+    {
+      img: 'https://fastly.4sqi.net/img/general/600x600/6357372_P37yC5wYFFEhCGHQnxu3Bvkiad7Vix75CR5irwo0lto.jpg',
+      title: 'Igreja Matriz',
+    },
+    {
+      img: 'http://camaraaguasbelas.pe.gov.br/v1/wp-content/uploads/2017/05/slider.png',
+      title: 'Águas Belas',
+    }
+  ];
