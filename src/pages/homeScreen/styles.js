b03ff7a02254a204a202ad97c1aefbc58c8c866c
Januacele <januacelevieira@hotmail.com>
2022-10-09 14:13:07 -0300

    feat: style: home  screen style

---
+++ src/pages/homeScreen/styles.js
@@ -0,0 +1,10 @@
+export const homeScreenStyles = {
+    wrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '20px',
+        marginLeft: '500px',
+        marginTop: '20px'
+    }
+}
