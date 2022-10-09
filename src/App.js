843014fcd4a28a44e54577917b182d7b6848a900
Januacele <januacelevieira@hotmail.com>
2022-10-09 14:26:07 -0300

    fix: importation routes att app

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import { useState } from "react";
 import UserContext from "../src/contexts/UserContext";
 import LoginScreen from "./pages/authPage/LoginScreen";
 import RegisterScreen from "./pages/authPage/RegisterScreen";
-import HomeScreen from "./pages/homePage/homeScreen/homeScreen";
-import InsertDistricts from "./pages/homePage/insertDistricts/InsertDistricts";
-import GeneralResults from './pages/homePage/generalResults/generalResults';
-import GraficResultsDistricts from './pages/homePage/graficResultsDistricts/graficResultsDistricts';
-import GraficResultsStreats from './pages/homePage/graficResultsStreats/graficResultsStreats';
-import InsertStreats from './pages/homePage/insertStreats/insertStreats';
+import HomeScreen from "./pages/homeScreen/homeScreen";
+import InsertDistricts from "./pages/insertDistricts/InsertDistricts";
+import GeneralResults from './pages/generalResults/generalResults';
+import GraficResultsDistricts from './pages/graficResultsDistricts/graficResultsDistricts';
+import GraficResultsStreats from './pages/graficResultsStreats/graficResultsStreats';
+import InsertStreats from './pages/insertStreats/insertStreats';
+import { ThemeProvider } from '@mui/material/styles';
+import { dashboardTheme } from './dashboardTheme';
 
 function App(){
     const [data, setData] = useState("");
     const [token, setToken] = useState("");
 
     return (
-        <UserContext.Provider value={{  token, setToken, data, setData }}>     
-            <BrowserRouter> 
-                <Routes>
-                    <Route path="/" element={<LoginScreen />}> </Route>
-                    <Route path="/register" element={<RegisterScreen />}> </Route>
-                    <Route path="/home" element={<HomeScreen />}> </Route>
-                    <Route path="/home/insertDistricts" element={<InsertDistricts />}> </Route>
-                    <Route path="/home/GeneralResults" element={<GeneralResults />}> </Route>
-                    <Route path="/home/graficResultsDistricts" element={<GraficResultsDistricts />}> </Route>
-                    <Route path="/home/GraficResultsStreats" element={<GraficResultsStreats />}> </Route>
-                    <Route path="/home/InsertStreats" element={<InsertStreats />}> </Route>
-                </Routes>
-            </BrowserRouter>
+        <UserContext.Provider value={{  token, setToken, data, setData }}>
+            <ThemeProvider theme={dashboardTheme}>
+                <BrowserRouter> 
+                    <Routes>
+                        <Route path="/" element={<LoginScreen />}> </Route>
+                        <Route path="/register" element={<RegisterScreen />}> </Route>
+                        <Route path="/home" element={<HomeScreen />}> 
+                            <Route path="/home/insertDistricts" element={<InsertDistricts />}> </Route>
+                            <Route path="/home/GeneralResults" element={<GeneralResults />}> </Route>
+                            <Route path="/home/graficResultsDistricts" element={<GraficResultsDistricts />}> </Route>
+                            <Route path="/home/GraficResultsStreats" element={<GraficResultsStreats />}> </Route>
+                            <Route path="/home/InsertStreats" element={<InsertStreats />}> </Route>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
         </UserContext.Provider>
     );
 }
