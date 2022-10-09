1041f7f668f65ace3a40d2e9259379360ffdc2a8
Januacele <januacelevieira@hotmail.com>
2022-10-09 14:13:45 -0300

    feat: insert home sceen

---
+++ src/pages/homeScreen/homeScreen.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import NavBarOption from '../../components/navBarOption';
+import { Outlet } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+import Header from '../../components/Header/Header';
+import { useLocation } from 'react-router-dom'; 
+import Box from '@mui/material/Box';
+import { homeScreenStyles } from './styles';
+import { StandardImageList } from './standardImageList';
+
+
+function HomeScreen() {
+  const [title, setTitle] = useState(null);
+  const location = useLocation();
+  
+  useEffect(() => {
+    const parsedTitle = location.pathname.replace(/\W/g, ' ');
+    setTitle(parsedTitle);
+  }, [location]);
+
+  return (
+    <Grid container>
+      <NavBarOption />
+      <Header title={title} />
+      <Outlet />
+
+      <Box sx={homeScreenStyles.wrapper}>
+        <StandardImageList />
+      </Box>
+    </Grid>
+    
+  );
+}
+
+export default HomeScreen;
