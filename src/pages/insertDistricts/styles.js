eefdc5095c8e79c8aaa3308f0d7281f666b0062b
Januacele <januacelevieira@hotmail.com>
2022-10-10 15:58:28 -0300

    fix: change page layout at insert dustrict style

--- src/pages/insertDistricts/styles.js
+++ src/pages/insertDistricts/styles.js
@@ -1,15 +1,43 @@
-export const cardHeaderStyles = {
-    wrapper: {
+export const iconsDistrictStreatStyle = {
+    box: {
+        padding: '12px',
+        backgroundColor: 'rgba(255, 255, 255, 0.9)!important',
+        boxShadow: '0px 2px 4px 2px #0000001a',
+        borderRadius: '3px',
+        margin: '10px 15px',
+        cursor: 'pointer',
+        width: '500px',
+        height: '50px',
+        zIndex: '-5'
+    },
+    iconsSet: {
+        color: 'rgba(0, 0, 0, 0.7)!important',
+        marginLeft: '20px',
+        marginTop: '-20px',
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingLeft: '20px',
-        paddingRight: '20px',
-        height: '65px',
-        backgroundColor: '#f5f5f5',
-        borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+        flexDirection: 'start',
+        cursor: 'pointer'
     },
-    addUserButton: {
-        fontSize: '1.05rem',
+
+    iconsEdit: {
+        color: 'rgba(0, 0, 0, 0.7)!important',
+        marginLeft: '430px',
+        marginTop: '-30px',
+        marginBottom: '5px',
+        cursor: 'pointer',
+        width: '22px'
     },
-}
+     iconsDelete: {
+        color: 'rgba(0, 0, 0, 0.7)!important',
+        marginLeft: '460px',
+        marginTop: '-300px',
+        marginBottom: '20px',
+        cursor: 'pointer',
+        width: '22px',
+    },
+    text: {
+        fontWeight: '600',
+        fontSize: '16px',
+        color: 'rgba(0, 0, 0, 0.7)!important',
+    }
+};
