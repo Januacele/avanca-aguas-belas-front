b166aa92b81bc82cdcdd0edb925d2b03a053f6de
Januacele <januacelevieira@hotmail.com>
2022-10-10 16:01:50 -0300

    fix: change layout at grafic results

--- src/pages/graficResultsStreats/graficResultsStreats.js
+++ src/pages/graficResultsStreats/graficResultsStreats.js
@@ -1,72 +1,70 @@
 import React from 'react';
+import { Chart } from "react-google-charts";
+import Grid from '@mui/material/Grid';
 import NavBarOption from "../../components/navBarOption";
-import { BasicCard } from "../../common/basicCard/BasicCard";
-import { SearchBar } from '../../common/searchBar/SearchBar';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import IconButton from '@mui/material/IconButton';
-import { CommonButton } from "../../common/commonButton";
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { GridWrapper } from "../../common/gridWrapper/GridWrapper";
-import { cardHeaderStyles } from "./styles";
+import Header from "../../components/Header/Header";
+import styled from "styled-components";
 
 const GraficResultsStreats = () => {
-    
-    const getHeader = () => {
-        const handleChange = (value) => {
-            console.log(value);
-        };
 
-        const addUser = () => {
-            console.log('click')
-        };
 
-        return (
-            <Box sx={cardHeaderStyles.wrapper}>
-                <SearchBar 
-                    placeholder="Search by email address, phone number, or user UID"
-                    onChange={(event) => handleChange(event.target.value)}
-                    searchBarWidth='720px'
-                />
-                <Box>
-                    <CommonButton 
-                        variant="contained"
-                        onClick={addUser}
-                        size="large"
-                        sx={cardHeaderStyles.addUserButton}
-                    >
-                        Add user
-                    </CommonButton>
-                    <IconButton>
-                        <RefreshIcon />
-                    </IconButton>
-                </Box>
-            </Box>
-        )
-    };
 
-    const getContent = () => (
-        <Typography 
-            align="center"
-            sx={{ margin: '40px 16px', color: 'rgba(0, 0, 0, 0.6)', fontSize: '1.3rem'}}
-        >
-            No users for this project yet
-        </Typography>
-    );
-
-    return (
-        <>
-        <NavBarOption />
-        <GridWrapper>
-            <BasicCard
-                header={getHeader()}
-                content={getContent()}
-            />
-        </GridWrapper>
-        </>
+   const data = [
+        ["Ruas", "Hours per Day"],
+        ["Rua1", 15],
+        ["Rua2", 8],
+        ["Rua3", 4],
+        ["Rua4", 2],
+        ["Rua5", 7],
+      ];
       
-    )
+     const options = {
+        title: "Exemplo - Ruas com Saneamento Básico",
+      };
+      
+    function CreatChart() {
+        return (
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"80%"}
+            height={"350px"}
+          />
+        );
+      }
+
+    return(
+      <>
+        <Header />
+        <NavBarOption />
+          <Container>
+              <h1>Página em Construção</h1>
+          </Container>     
+        <Grid style={{marginLeft: '400px', marginTop: '20px'}}>
+          <CreatChart />
+        </Grid>
+      </>
+        
+    );
 }
+
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    width: 100%;
+    height: 100%;
+
+    h1{
+        font-size: 28px;
+
+    }
+`;
+
 
 export default GraficResultsStreats
 
